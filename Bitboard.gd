1daf0905c138b9f2cc1d98c5d34709b27c27088d
Pablops2219 <141527850+Pablops2219@users.noreply.github.com>
2024-09-19 21:52:56 +0200

    bitboard
    Creo que he hecho el bitboard

--- Bitboard.gd
+++ Bitboard.gd
@@ -1,104 +1,14 @@
 extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-var white_pieces = [0, 0, 0, 0, 0, 0]
-var black_pieces = [0, 0, 0, 0, 0, 0]
-
-
-func clear_bitboard():
-	for i in range(white_pieces.size()):
-		white_pieces[i] = 0
-	for i in range(black_pieces.size()):
-		black_pieces[i] = 0
-
-func set_board(whites: Array, blacks: Array):
-	for i in range(whites.size()):
-		white_pieces[i] = whites[i]
-	for i in range(blacks.size()):
-		black_pieces[i] = blacks[i]
-
-func init_bitboard(fen: String):
-	clear_bitboard()
-	var fen_split = fen.split(" ")
-	for i in fen_split[0]:
-		if i == '/':
-			continue
-		if i.is_digit():
-			var shift_amount = int(i)
-			left_shift(shift_amount)
-			continue
-		left_shift(1)
-		if i.is_upper():
-			white_pieces[DataHandler.fen_dict[i.to_lower()]] |= 1
-		else:
-			black_pieces[DataHandler.fen_dict[i]] |= 1
-	print("Bitboard init successfully")
-
-func get_black_bitboard() -> int:
-	var ans = 0
-	for i in black_pieces:
-		ans |= i
-	return ans
-
-func get_white_bitboard() -> int:
-	var ans = 0
-	for i in white_pieces:
-		ans |= i
-	return ans
-
-func left_shift(shift_amount: int):
-	for piece in range(black_pieces.size()):
-		black_pieces[piece] <<= shift_amount
-	for piece in range(white_pieces.size()):
-		white_pieces[piece] <<= shift_amount
-
-
-#
-	#public ulong GetWhiteBitboard(){
-		#ulong ans = 0;
-		#foreach(ulong i in whitePieces){
-			#ans |= i;
-		#}
-		#return ans;
-	#}
 #
 #
-	#public void ClearBitboard(){
-		#Array.Clear(whitePieces);
-		#Array.Clear(blackPieces);
-	#}
 #
-	#public void SetBoard(ulong[] Whites, ulong[] Blacks){
-		#Array.Copy(Whites, whitePieces, Whites.Length);
-		#Array.Copy(Blacks, blackPieces, Blacks.Length);
-	#}
 #
-	#public void InitBitBoard(string fen){
-		#ClearBitboard();
-		#string[] fen_split = fen.Split(" ");
-		#foreach (char i in fen_split[0]){
-			#if (i.Equals('/'))
-				#continue;
-			#if (Char.IsDigit(i)){
-				#int shiftAmount = int.Parse(i.ToString());
-				#LeftShift(shiftAmount);
-				#continue;
-			#}
-			#LeftShift(1);
-			#if(Char.IsUpper(i)){
-				#whitePieces[DataHandlerCS.FenDict[Char.ToLower(i)]] |= 1UL;
-			#}else{
-				#blackPieces[DataHandlerCS.FenDict[i]] |= 1UL;
-			#}
-		#}
-		#GD.Print("Bitboard init successfully");
-	#}
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#pass
+#
+#
+#func _on_button_2_pressed():
--- GUI.gd
+++ GUI.gd
@@ -99,3 +99,72 @@ func parse_fen(fen : String):
 func _on_button_pressed():
 	parse_fen(fen)
 	#set_filtro_tablero(1023)
+
+func _on_button_2_pressed():
+	init_bitboard(fen) 
+	set_filtro_tablero(get_black_bitboard())
+	get_white_bitboard()
+	print("?")
+
+enum PieceNames { BISHOP, KING, KNIGHT, PAWN, QUEEN, ROOK }
+var fen_dict = {
+	'b': 0,
+	'k': 1,
+	'n': 2,
+	'p': 3,
+	'q': 4,
+	'r': 5
+}
+
+
+var white_pieces = [0, 0, 0, 0, 0, 0]
+var black_pieces = [0, 0, 0, 0, 0, 0]
+
+
+func clear_bitboard():
+	for i in range(white_pieces.size()):
+		white_pieces[i] = 0
+	for i in range(black_pieces.size()):
+		black_pieces[i] = 0
+
+func set_board(whites: Array, blacks: Array):
+	for i in range(whites.size()):
+		white_pieces[i] = whites[i]
+	for i in range(blacks.size()):
+		black_pieces[i] = blacks[i]
+
+func init_bitboard(fen: String):
+	clear_bitboard()
+	var fen_split = fen.split(" ")
+	for i in fen_split[0]:
+		if i == '/':
+			continue
+		if i.is_valid_int():
+			var shift_amount = int(i)
+			left_shift(shift_amount)
+			continue
+		left_shift(1)
+		if i >= 'A' and i <= 'Z':
+			white_pieces[fen_dict[i.to_lower()]] |= 1
+		else:
+			black_pieces[fen_dict[i]] |= 1
+	print("Bitboard init successfully")
+
+func get_black_bitboard() -> int:
+	var ans = 0
+	for i in black_pieces:
+		ans |= i
+	return ans
+
+func get_white_bitboard() -> int:
+	var ans = 0
+	for i in white_pieces:
+		ans |= i
+		print(ans)
+	return ans
+
+func left_shift(shift_amount: int):
+	for piece in range(black_pieces.size()):
+		black_pieces[piece] <<= shift_amount
+	for piece in range(white_pieces.size()):
+		white_pieces[piece] <<= shift_amount
